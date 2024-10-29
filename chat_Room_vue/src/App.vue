73afc0776fb2f7e35f87268ed35bee2050d00f19
赵信勇 <AyongNice@163.com>
2024-10-29 23:46:50 +0800

    前端项目

---
+++ chat_Room_vue/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div  id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+.header {
+  height: 44px;
+  width: 100%;
+  position: fixed;
+  z-index: 999;
+  background: #ebebeb;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  /*margin-top: 60px;*/
+}
+.action_sheet_box {
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+  position: fixed;
+  bottom: 57px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
+
+
+
+// WEBPACK FOOTER //
+// App.vue
